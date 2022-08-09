a803ed7e68e521dd7909dca2bf3b17e1843c40d5
Mateusz Budzisz <ElectroluxV2@gmail.com>
2022-08-10 00:51:01 +0200

    Changed build target to es2022 (#23)

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
-export default {
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  esbuild: {
+    // mangleProps: new RegExp('.*_', 'gm'), TODO: Add TS properties transformer
+  },
   build: {
-    target: 'es2021',
+    minify: true,
+    target: 'es2022',
     polyfillModulePreload: false
   }
-}
+})

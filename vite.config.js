9e7dee7f26a5f541202c2976c8b12ca319f50164
ElectroluxV2 <electroluxv2@gmail.com>
2022-08-13 20:25:47 +0200

    Test
    Signed-off-by: ElectroluxV2 <electroluxv2@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
+import typescript from "@rollup/plugin-typescript";
+import ttypescript from "ttypescript";
 
 export default defineConfig({
   esbuild: {
-    // mangleProps: new RegExp('.*_', 'gm'), TODO: Add TS properties transformer
+    mangleProps: new RegExp('_.*', 'gm')
   },
   build: {
     minify: true,
     target: 'es2022',
     polyfillModulePreload: false
-  }
-})
+  },
+  plugins: [
+    typescript({
+      typescript: ttypescript
+    })
+  ]
+});
